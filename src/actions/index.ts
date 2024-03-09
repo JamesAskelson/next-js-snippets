7a18327b48a3f1225153036c94415fe12eb3f357
JamesAskelson <jamesaskelson630@gmail.com>
2024-03-09 16:52:04 -0600

    error-validations-plus-custom-nextjs-error-page

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { db } from "@/db";
+import { error } from "console";
 import { redirect } from "next/navigation";
 
 export async function editSnippet(id: number, code: string) {
@@ -16,4 +17,47 @@ export async function deleteSnippet(id: number) {
         where: {id}
     })
     redirect('/')
+}
+
+export async function createSnippet(formState: {message: string},formData: FormData) {
+    try {
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get('title')
+    const code = formData.get('code')
+
+    if(typeof title !== 'string' || title.length < 3) {
+        return {
+            message: 'Title must be longer'
+        }
+    }
+
+    if(typeof code !== 'string' || code.length < 10) {
+        return {
+            message: 'Code must be longer'
+        }
+    }
+
+    // if valid, create new record in the db
+    const snippet = await db.snippet.create({
+        data: {
+            title,
+            code
+        }
+    })
+    
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                message: err.message
+            }
+        } else {
+            return {
+                message: 'Something went wrong...'
+            }
+        }
+    }
+
+    // return to the homepage/new snippet page
+
+    redirect('/');
 }
